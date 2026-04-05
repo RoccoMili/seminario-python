f250738062fd74c8e22376f09d8890197041b651
Rocco <roccomili.rm@gmail.com>
2026-04-05 14:12:06 -0300

    hecho el ejercicio 08

---
+++ Práctico 02/src/caesar_cipher.py
@@ -0,0 +1,13 @@
+# Me pareció buen uso de un módulo estándar, porque al trabajar con
+# valores ASCII necesitaba algo de este estilo para hacer un translate
+# (que era mi idea original). Tuve que investigar.
+import string
+
+def cipher(message, shift):
+    alpha_shift = shift % 26
+    lowercase = string.ascii_lowercase
+    uppercase = string.ascii_uppercase
+    shifted_lower = lowercase[alpha_shift:] + lowercase[:alpha_shift]
+    shifted_upper = uppercase[alpha_shift:] + uppercase[:alpha_shift]
+    transtable = str.maketrans(lowercase + uppercase, shifted_lower + shifted_upper)
+    return message.translate(transtable)
